59af9b5bc3621f6f77da333c3a070fdf05545e39
z.aqib <zuha.aqib@gmail.com>
2024-05-27 20:58:23 +0500

    Add Exercise 01 to Chapter 01

---
+++ Chapter 01 - Introduction To Computers & Java/Exercise01/C1e1.java
@@ -0,0 +1,13 @@
+package Exercise01;
+
+public class C1e1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun.");
+
+    }
+
+}
---
+++ Chapter 01 - Introduction To Computers & Java/Exercise01/README.md
@@ -0,0 +1,3 @@
+# Exercise 01
+
+1.1 (Display three messages) Write a program that displays Welcome to Java, Welcome to Computer Science, and Programming is fun.
